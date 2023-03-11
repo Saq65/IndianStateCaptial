b47cf59ea6e2efdf671e85dfc0d81ab20d01c4fd
saqlain shaikh <118292650+Saq65@users.noreply.github.com>
2023-03-11 14:23:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,339 @@
+var a;
+function state1(){
+    document.getElementById('cap').innerHTML="Amravati-अमरावती";
+    if(a==1){
+        document.getElementById('cap').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state2(){
+    document.getElementById('cap2').innerHTML="Itanagar-ईतानगर";
+    if(a==1){
+        document.getElementById('cap2').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap2').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state3(){
+    document.getElementById('cap3').innerHTML="Dispur-दिसपुर";
+    if(a==1){
+        document.getElementById('cap3').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap3').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state4(){
+    document.getElementById('cap4').innerHTML="Patna-पटना";
+    if(a==1){
+        document.getElementById('cap4').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap4').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state5(){
+    document.getElementById('cap5').innerHTML="Raipur-रायपुर";
+    if(a==1){
+        document.getElementById('cap5').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap5').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state6(){
+    document.getElementById('cap6').innerHTML="Panaji-पणजी ";
+    if(a==1){
+        document.getElementById('cap6').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap6').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state7(){
+    document.getElementById('cap7').innerHTML="Ghandhinagar-गाँधीनगर";
+    if(a==1){
+        document.getElementById('cap7').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap7').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state8(){
+    document.getElementById('cap8').innerHTML="Chandigarh-चंडीगढ़";
+    if(a==1){
+        document.getElementById('cap8').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap8').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state9(){
+    document.getElementById('cap9').innerHTML="Shimla-शिमला";
+    if(a==1){
+        document.getElementById('cap9').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap9').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state10(){
+    document.getElementById('cap10').innerHTML="Srinagar/Jammu श्रीनगर/जम्मु";
+    if(a==1){
+        document.getElementById('cap10').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap10').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state11(){
+    document.getElementById('cap11').innerHTML="Ranchi-रांची";
+    if(a==1){
+        document.getElementById('cap11').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap11').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state12(){
+    document.getElementById('cap12').innerHTML="Bengaluru-बैंगलोर";
+    if(a==1){
+        document.getElementById('cap12').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap12').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state13(){
+    document.getElementById('cap13').innerHTML="Thiruvananthapuram तिरुवनंतपुरम";
+    if(a==1){
+        document.getElementById('cap13').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap13').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state14(){
+    document.getElementById('cap14').innerHTML="Bhopal-भोपाल";
+    if(a==1){
+        document.getElementById('cap14').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap14').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state15(){
+    document.getElementById('cap15').innerHTML="Mumbai-मुंबई";
+    if(a==1){
+        document.getElementById('cap15').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap15').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state16(){
+    document.getElementById('cap16').innerHTML="Imphal-इंफाल";
+    if(a==1){
+        document.getElementById('cap16').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap16').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state17(){
+    document.getElementById('cap17').innerHTML="Shilong-शिलांग";
+    if(a==1){
+        document.getElementById('cap17').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap17').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state18(){
+    document.getElementById('cap18').innerHTML="Aizawl-आइजोल";
+    if(a==1){
+        document.getElementById('cap18').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap18').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state19(){
+    document.getElementById('cap19').innerHTML="Kohima-कोहिमा";
+    if(a==1){
+        document.getElementById('cap19').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap19').style.display="none";
+        return a=1;
+    }
+}
+var a;
+function state20(){
+    document.getElementById('cap20').innerHTML="Bhubaneswar-भुवनेश्वर";
+    if(a==1){
+        document.getElementById('cap20').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap20').style.display="none";
+        return a=1;
+    }
+}
+function state21(){
+    document.getElementById('cap21').innerHTML="Chandigarh-चंडीगढ़";
+    if(a==1){
+        document.getElementById('cap21').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap21').style.display="none";
+        return a=1;
+    }
+}
+function state22(){
+    document.getElementById('cap22').innerHTML="Jaipur-जयपुर";
+    if(a==1){
+        document.getElementById('cap22').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap22').style.display="none";
+        return a=1;
+    }
+}
+function state23(){
+    document.getElementById('cap23').innerHTML="Gangtok-गंगतोक";
+    if(a==1){
+        document.getElementById('cap23').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap23').style.display="none";
+        return a=1;
+    }
+}
+function state24(){
+    document.getElementById('cap24').innerHTML="Chennai-चैन्नई";
+    if(a==1){
+        document.getElementById('cap24').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap24').style.display="none";
+        return a=1;
+    }
+}
+function state25(){
+    document.getElementById('cap25').innerHTML="Hydrabad-हैदराबाद";
+    if(a==1){
+        document.getElementById('cap25').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap25').style.display="none";
+        return a=1;
+    }
+}
+function state26(){
+    document.getElementById('cap26').innerHTML="Agartala-अगरतला";
+    if(a==1){
+        document.getElementById('cap26').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap26').style.display="none";
+        return a=1;
+    }
+}
+function state27(){
+    document.getElementById('cap27').innerHTML="Lucknow-लखनऊ";
+    if(a==1){
+        document.getElementById('cap27').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap27').style.display="none";
+        return a=1;
+    }
+}
+function state28(){
+    document.getElementById('cap28').innerHTML="Dehradun-देहरादून";
+    if(a==1){
+        document.getElementById('cap28').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap28').style.display="none";
+        return a=1;
+    }
+}
+function state29(){
+    document.getElementById('cap29').innerHTML="Kolkata-कोलकाता";
+    if(a==1){
+        document.getElementById('cap29').style.display="block";
+        return a=0;
+    }
+    else {
+        document.getElementById('cap29').style.display="none";
+        return a=1;
+    }
+}
